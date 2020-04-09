4a2e897368bf481caa7ef72b39a5bc15827bccf1
Gilps <48761839+1958gilps@users.noreply.github.com>
2020-04-09 12:58:20 -0400

    Add files via upload

---
+++ src/client/views/index.js
@@ -0,0 +1,16 @@
+import { checkForQuestion } from './js/questionChecker'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+export {checkForQuestion, handleSubmit};
+
+// console.log(checkForName);
+
+// alert("Janny is awesome !!!")
+
+// console.log("CHANGE!!");
